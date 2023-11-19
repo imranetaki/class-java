071b31a53e554ced2cdd75d44ebb710b04c1d387
imranetaki <146876470+imranetaki@users.noreply.github.com>
2023-11-19 22:08:57 +0100

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    String firstname ;
+    String lastname ;
+    short age ;
+    short salary ;
+    void email(){
+        System.out.println(firstname + "." + lastname + "@company.com");
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee firstEmployee = new Employee();
+        firstEmployee.firstname = "Ali" ;
+        firstEmployee.lastname = "Mohamed" ;
+        firstEmployee.age = 30 ;
+        firstEmployee.salary = 15000 ;
+
+        firstEmployee.email();
+
+
+    }
+}
